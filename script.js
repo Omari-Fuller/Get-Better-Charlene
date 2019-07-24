8fb5a609270a80330a1bd53b0a688c250b39f953
Omari-Fuller <43542826+Omari-Fuller@users.noreply.github.com>
2019-07-24 19:31:17 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$("button").dblclick(function() {
+$("img").fadeIn();
+$("body").css("background-color","blue"); 
+$("body").append("<p><strong>Heyyyy Charlene is Mari mar mar. I know your in the hospital and I want to say thanks for all the affection you showed me over the years. I remember yout told me you used to hold me and watch me when my mom had work. Maybe thats why were so close. Also im super flattered by how you want ken j and reese to follow my footsteps and ask me for advice when theyre my age. They're multiple options however you chose me and Im super grateful I will treat both of them like theyre my little brothers. I love you so much and when you get out the hospital im going to give you the biggest hug and best hug ever. I love you your so full of life and energy and like me love family more than anything else. Your nephew / friend -Mari :)> </p>");
+
+});
+
+    
+    
+
